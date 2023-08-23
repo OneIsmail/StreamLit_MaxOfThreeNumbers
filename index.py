c635c263807e18e2989c570d07a4558cb4277036
OneIsmail <38859183+OneIsmail@users.noreply.github.com>
2023-08-23 14:43:29 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,6 @@
+import streamlit as st
 
+
+number1 = st.text_input('Enter first number: ', '1')
+number2 = st.text_input('Enter second number: ', '2')
+number3 = st.text_input('Enter third number: ', '3')
