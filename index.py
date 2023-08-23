e012f023b19d3c852b7fd2323638307dcbf59ac6
OneIsmail <38859183+OneIsmail@users.noreply.github.com>
2023-08-23 14:50:38 +0530

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ number2 = st.number_input('Enter second number: ')
 number3 = st.number_input('Enter third number: ')
 if (number1 >= number2) and (number1 >= number3):
   st.write('The maximum number is ', number1)
-elif (number2 >= number1) and (number2 >= number):
+elif (number2 >= number1) and (number2 >= number3):
   st.write('The maximum number is ', number2)
 else:
   st.write('The maximum number is ', number3)
